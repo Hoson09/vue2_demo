16619730ecdaccb50029bad5a08f5469083e166a
huyi <huygeorge@163.com>
2022-06-19 15:11:15 +0800

    first commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <tableComponent />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import tableComponent from "@/components/tableComponent.vue";
 
 export default {
   name: "HomeView",
   components: {
-    HelloWorld,
+    tableComponent,
   },
 };
 </script>
